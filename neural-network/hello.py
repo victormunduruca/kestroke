4e91bcfe22383dd7a641cb9af6f614ae5798da58
Victor Munduruca <victormunduruca@gmail.com>
2018-05-13 00:31:06 -0300

    Divisão entre treino e teste, ok.

--- neural-network/hello.py
+++ neural-network/hello.py
@@ -12,20 +12,21 @@ ds = SupervisedDataSet(48, 1)
 for input in lines:
     input = input.split(",")
     [float(i) for i in input if i != '']
-    print(input[0])
     ds.addSample(input[1:], input[0])
 
-
+train, test = ds.splitWithProportion(0.25)
 
 
 nn = buildNetwork(48, 1, 1, bias=True)
 
 print(nn)
-trainer = BackpropTrainer(nn, ds, learningrate=0.01)
+trainer = BackpropTrainer(nn, train, learningrate=0.001)
 print(trainer.trainUntilConvergence())
 
-
-print(nn.activate([774,1594,576,2187,536,422,370,343,485,1017,335,344,344,257,406,235,640,568,297,313,258,1000,483,266,453,1336,711,313,2188,1470,773,922,420,297,297,520,282,828,683,344,363,500,1172,305,422,641,816,844]))
+for inp, targ in test:
+    mytarg = nn.activate(inp)
+    print(mytarg - targ)
+#print(nn.activate([774,1594,576,2187,536,422,370,343,485,1017,335,344,344,257,406,235,640,568,297,313,258,1000,483,266,453,1336,711,313,2188,1470,773,922,420,297,297,520,282,828,683,344,363,500,1172,305,422,641,816,844]))
 
 
 
