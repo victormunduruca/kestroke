3bc7f6810521d8174f13e24231bb72ec0969b717
Victor Munduruca <victormunduruca@gmail.com>
2018-05-13 23:55:32 -0300

    Dando o mesmo resultado diversas vezes

--- neural-network/hello.py
+++ neural-network/hello.py
@@ -3,30 +3,36 @@ from pybrain.datasets import SupervisedDataSet
 from pybrain.supervised.trainers import BackpropTrainer
 
 
-with open('latencias_completo.txt') as f:
+
+with open('latencias_normalizado.txt') as f:
     lines = f.readlines()
 
 
 ds = SupervisedDataSet(48, 1)
 
 for input in lines:
-    input = input.split(",")
-    [float(i) for i in input if i != '']
-    ds.addSample(input[1:], input[0])
+     input = input.split(",")
+     [float(i) for i in input if i != '']
+     ds.addSample(input[1:], input[0])
 
 train, test = ds.splitWithProportion(0.25)
 
 
 nn = buildNetwork(48, 1, 1, bias=True)
 
+
 print(nn)
 trainer = BackpropTrainer(nn, train, learningrate=0.001)
-print(trainer.trainUntilConvergence())
+#print(trainer.trainUntilConvergence())
+
+for i in xrange(1000):
+    print(trainer.train())
 
 for inp, targ in test:
-    mytarg = nn.activate(inp)
-    print(mytarg - targ)
-#print(nn.activate([774,1594,576,2187,536,422,370,343,485,1017,335,344,344,257,406,235,640,568,297,313,258,1000,483,266,453,1336,711,313,2188,1470,773,922,420,297,297,520,282,828,683,344,363,500,1172,305,422,641,816,844]))
+     mytarg = nn.activate(inp)
+     print(mytarg)
+     print(targ)
+     print()
 
 
 
