7d3574fc89ae8752c6c59a4ad1aad37a15c48d4c
victormunduruca <victormunduruca@gmail.com>
2018-08-20 18:45:33 -0300

    getting validation and training errors

--- neural-network/hello.py
+++ neural-network/hello.py
@@ -3,7 +3,7 @@ from pybrain.datasets import SupervisedDataSet
 from pybrain.supervised.trainers import BackpropTrainer
 from pybrain.structure.modules import SigmoidLayer
 
-
+print('hello world')
 with open('latencias_normalizado.txt') as f:
     lines = f.readlines()
 
@@ -18,17 +18,30 @@ for input in lines:
 train, test = ds.splitWithProportion(0.25)
 
 
+
+
 nn = buildNetwork(48, 20, 1, bias=True, outclass = SigmoidLayer)
 nn.reset()
 
 trainer = BackpropTrainer(nn, train, learningrate=0.001)
 
-for i in xrange(3000):
-    print(trainer.train())
+training_errors, validation_errors = trainer.trainUntilConvergence()
 
-for inp, targ in test:
-    mytarg = nn.activate(inp)
-    print(mytarg)
-    print(targ)
-    print 
+print('erros de treino ------------------------------------------------------------')
+for value in training_errors:
+    #print(training_errors)
+    print(value)
+print('erros de validacao ----------------------------------------------------------')
+for val in validation_errors:
+    #print(training_errors)
+    print(val)
+
+#for i in xrange(3000):
+ #   print(trainer.train())
+
+
+#for inp, targ in test:
+#    mytarg = nn.activate(inp)
+  #  print(mytarg)
+  # print(targ)
 
